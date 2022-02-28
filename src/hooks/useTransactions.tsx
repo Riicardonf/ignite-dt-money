19cb678a9d2f9304329ee99fa66ccf1bd974f184
Ricardo Nascimento <ricardobatistanf@gmail.com>
2022-02-28 11:52:44 -0300

    Refactoring code context

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import { api } from "services/api";
 
 
@@ -22,7 +22,7 @@ interface TransactionsContextData{
   createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -53,5 +53,11 @@ export function TransactionsProvider({children}: TransactionsProviderProps){
       {children}
     </TransactionsContext.Provider>
   )
+  
+}
 
+export function useTransactions(){
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
